7d1b8f90c544367634a1b225f5bc4b5d02c9621b
Vitalii Tomenko <vitalikxxx985@gmail.com>
2024-04-04 15:39:55 +0200

    created skills

---
+++ src/features/personalHomePage/PersonalHomepage/skillsData.js
@@ -0,0 +1,33 @@
+export const skills = [
+    "Semantic & accessible HTML",
+    "Error handling",
+    "Git",
+    "Responsive Web Design",
+    "React",
+    "GitHub Pull Request & Review",
+    "JavaScript: ES6+",
+    "React Hooks",
+    "NPM",
+    "Working with API (fetch, axios)",
+    "Redux (Toolkit)",
+    "Immutabulity",
+    "Promises, Async/Await",
+    "Redux-Saga",
+    "Markdown",
+    "Debugging",
+    "React Router",
+    "Scrum",
+    "CSS Flexbox",
+    "CSS Grid",
+    "Trello",
+    "CSS BEM convention",
+];
+
+export const nextSkills = [
+    "JS classes",
+    "Unit testing",
+    "Node.js",
+    "TypeScript",
+    "Cypress",
+    "React Context",
+];
